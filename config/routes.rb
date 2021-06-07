913c62891c220bb7678fe6db98ecebe34212112a
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-06-07 01:36:11 +0000

    [add]User & Admin model

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
   scope module: :public do
     root 'homes#top'
   end
+  namespace :admin do
+    devise_for :admins, controllers: {
+      sessions:      'admin/admins/sessions',
+      passwords:     'admin/admins/passwords',
+      registrations: 'admin/admins/registrations'
+    }
+  end
+
+  scope module: :public do
+    devise_for :users, controllers: {
+      sessions:      'public/users/sessions',
+      passwords:     'public/users/passwords',
+      registrations: 'public/users/registrations'
+    }
+  end
 end
