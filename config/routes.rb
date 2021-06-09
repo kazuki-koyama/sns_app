babd464167a881455ab9d06d41341fd345aefec0
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-06-09 07:52:04 +0000

    [add] Relationship model

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@ Rails.application.routes.draw do
       registrations: 'public/users/registrations'
     }
     root 'homes#top'
-    resources :users, only: [:show, :index, :edit, :update]
+    resources :users, only: [:show, :index, :edit, :update] do
+      resources :relationships, only: [:create, :destroy]
+    	get 'followings' => 'relationships#followings', as: 'followings'
+    	get 'followers' => 'relationships#followers', as: 'followers'
+    end
     resources :posts, only: [:new, :create, :index, :edit, :update, :destroy] do
       resource  :likes, only: [:create, :destroy]
       resources :comments, only: [:create, :destroy]
