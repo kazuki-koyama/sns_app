0d664404987960b558d0c7155b3c9eac3232effa
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-06-09 04:57:36 +0000

    [add] feature-comment with ajax

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-
   belongs_to :user
-  has_many :likes, dependent: :destroy
+  has_many :likes,    dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   attachment :before_image
   attachment :after_image
@@ -12,5 +12,9 @@ class Post < ApplicationRecord
 
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
+  end
+
+  def set_date
+    created_at.strftime('%Y/%m/%d')
   end
 end
