96310362fbc6afca360b07d8782181ceec6821eb
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-06-10 11:04:12 +0000

    [add] create & update method for hashtag

--- app/models/post.rb
+++ app/models/post.rb
@@ -24,4 +24,23 @@ class Post < ApplicationRecord
   def set_date
     created_at.strftime('%Y/%m/%d')
   end
+
+  after_create do
+    post = Post.find_by(id: id)
+    hashtags = hashbody.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
+    hashtags.uniq.map do |hashtag|
+      tag = Hashtag.find_or_create_by(hashname: hashtag.downcase.delete('#'))
+      post.hashtags << tag
+    end
+  end
+
+  before_update do
+    post = Post.find_by(id: id)
+    post.hashtags.clear
+    hashtags = hashbody.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
+    hashtags.uniq.map do |hashtag|
+      tag = Hashtag.find_or_create_by(hashname: hashtag.downcase.delete('#'))
+      post.hashtags << tag
+    end
+  end
 end
