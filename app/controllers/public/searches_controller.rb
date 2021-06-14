19f43a7fd19f9b1eb3ee2679b154638e981bde9d
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-06-14 01:04:45 +0000

    [add] feature-search for hashtags & users

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,2 +1,15 @@
 class Public::SearchesController < ApplicationController
+  def search
+    if (params[:keyword]) == '#' || (params[:keyword]) == ""
+      redirect_back(fallback_location: root_path)
+    elsif (params[:keyword]).include?('#')
+      @posts = Hashtag.search(params[:keyword]).order('created_at DESC')
+      # 完全一致以外はリダイレクト
+      redirect_back(fallback_location: root_path) if @posts[0].nil?
+      @hashtag = (params[:keyword])
+      @comment = Comment.new
+    else
+      @users = User.search(params[:keyword]).order('created_at DESC')
+    end
+  end
 end
--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -2,4 +2,14 @@ class Hashtag < ApplicationRecord
   validates :hashname, presence: true, length: { maximum: 50 }
   has_many :hashtag_posts, dependent: :destroy
   has_many :posts, through: :hashtag_posts
+
+  def self.search(keyword)
+    if keyword != '#'
+      hashtag = Hashtag.where(hashname: keyword.downcase.delete('#'))
+      return none if hashtag[0].nil?
+      # 部分一致検索の場合
+      # hashtag = Hashtag.where('hashname LIKE(?)' , "%#{keyword.downcase.delete('#')}%")
+      hashtag[0].posts
+    end
+  end
 end
