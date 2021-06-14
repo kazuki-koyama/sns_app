bf5e5d468e91f42cfaa76e3f3d729f587393a6da
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-06-14 23:31:09 +0000

    [fix] bxslider option

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,15 +29,26 @@ $(document).on('turbolinks:load', function() {
   });
 });
 
-// $(document).on('turbolinks:load', function() {
-$(document).ready(function() {
+// $(document).ready(function() {
+$('.slider').on('click', function() {
   $('.bxslider').bxSlider({
-    pager: true,
-    controls: true,
+    // pager: true,
+    // controls: true,
+    // moveSlides: 1,
+    // maxSlides: 2,
     nextText: "→",
     prevText: "←",
-    moveSlides: 1,
-    maxSlides: 2,
     slideWidth: 300
   });
 });
+
+// let slider1 = new SliderInit('.slider1','.slide-counter1','.slide-counter1 span');
+// // モーダルの設定
+// let sliderModalSet1 = {
+// el: '.sliderModal1',
+// sliderModaleEl: '.slider1 li a',
+// sliderModaleWrap: '.sliderModalWrap1',
+// mode: 'fade',
+// speed: 400
+// }
+// let sliderModal1 = new SliderModalInit(sliderModalSet1);
