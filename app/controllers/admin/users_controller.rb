ce3851a3e30441a58caef4c48c4232b61af688c6
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-06-16 04:25:19 +0000

    [add] admin/users#index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,31 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
-    
+    @users = User.all.order(created_at: :desc)
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+    @comment = Comment.new
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "Updated successfully"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
