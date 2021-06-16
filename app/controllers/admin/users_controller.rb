6e7b7c4c820c602f7fb292d7b4b55eda83a88464
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-06-16 06:19:40 +0000

    [add] admin/users#show#update

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,30 +2,25 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.all.order(created_at: :desc)
+    @users = User.page(params[:page]).reverse_order.per(15)
   end
 
   def show
     @user = User.find(params[:id])
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @posts = @user.posts
-    @comment = Comment.new
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user), notice: "Updated successfully"
+      redirect_to admin_user_path(@user), notice: "Updated successfully"
     else
-      render "edit"
+      render "show"
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
+    params.require(:user).permit(:is_valid)
   end
 end
