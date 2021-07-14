50aace5c7aeb2c5f35fce463540728e791aa67b9
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-07-14 13:41:54 +0000

    [fix] refile.rb

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -8,6 +8,6 @@ aws = {
 Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
 #Refile.store = Refile::S3.new(prefix: 'store', **aws)	# S3バケットにstore/フォルダを作成(ファイルはその配下に保存)
 
-Refile.backends['before_image'] = Refile::S3.new(prefix: 'before_image', **aws)
-Refile.backends['after_image'] = Refile::S3.new(prefix: 'after_image', **aws)
-Refile.backends['profile_image'] = Refile::S3.new(prefix: 'profile_image', **aws)
+Refile.backends['before_image'] = Refile::S3.new(prefix: 'store', **aws)
+Refile.backends['after_image'] = Refile::S3.new(prefix: 'store', **aws)
+Refile.backends['profile_image'] = Refile::S3.new(prefix: 'store', **aws)
